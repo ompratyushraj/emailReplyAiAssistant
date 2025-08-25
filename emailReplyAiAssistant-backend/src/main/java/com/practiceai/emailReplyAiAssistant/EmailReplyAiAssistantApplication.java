ba9d1a17892e6132197dfc74045fe14572dad30c
ompratyushraj <ompratyushraj02@gmail.com>
2025-08-25 14:50:17 +0530

    firstCommit25082025250

---
+++ emailReplyAiAssistant-backend/src/main/java/com/practiceai/emailReplyAiAssistant/EmailReplyAiAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.practiceai.emailReplyAiAssistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailReplyAiAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailReplyAiAssistantApplication.class, args);
+	}
+
+}
---
+++ emailReplyAiAssistant-backend/src/test/java/com/practiceai/emailReplyAiAssistant/EmailReplyAiAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practiceai.emailReplyAiAssistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailReplyAiAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
