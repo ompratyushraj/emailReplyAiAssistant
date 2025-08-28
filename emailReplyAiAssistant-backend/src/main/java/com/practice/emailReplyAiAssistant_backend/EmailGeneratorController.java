f785b3f65a36e5bd641c5e5f6aab7364d4063fcf
ompratyushraj <ompratyushraj02@gmail.com>
2025-08-28 23:51:11 +0530

    28082025

--- emailReplyAiAssistant-backend/src/main/java/com/practice/emailReplyAiAssistant_backend/EmailGeneratorController.java
+++ emailReplyAiAssistant-backend/src/main/java/com/practice/emailReplyAiAssistant_backend/EmailGeneratorController.java
@@ -1,5 +1,6 @@
 package com.practice.emailReplyAiAssistant_backend;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +16,7 @@ public class EmailGeneratorController {
     private final EmailGeneratorService emailGeneratorService;
 
     @PostMapping("/generate")
-    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest) throws JsonProcessingException {
         String response = emailGeneratorService.generateEmailReply(emailRequest);
         return ResponseEntity.ok("");
     }
--- emailReplyAiAssistant-backend/src/main/java/com/practice/emailReplyAiAssistant_backend/EmailGeneratorService.java
+++ emailReplyAiAssistant-backend/src/main/java/com/practice/emailReplyAiAssistant_backend/EmailGeneratorService.java
@@ -1,5 +1,8 @@
 package com.practice.emailReplyAiAssistant_backend;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,15 +12,16 @@ public class EmailGeneratorService {
 
     private final WebClient webClient;
 
-    public EmailGeneratorService(WebClient.Builder webClienBuilder,
-            @Value() String geminiApiKey,@Value() String baseUrl,) {
-        this.webClient = webClient;
-        this.apiKey = apiKey;
+    public EmailGeneratorService(WebClient.Builder webClientBuilder,
+                                 @Value("${gemini.api.url}") String geminiApiKey,
+                                 @Value("${gemini.api.key}") String baseUrl) {
+        this.apiKey = geminiApiKey;
+        this.webClient = webClientBuilder.baseUrl(baseUrl).build();
     }
 
     private final String apiKey;
 
-    public String generateEmailReply(EmailRequest emailRequest) {
+    public String generateEmailReply(EmailRequest emailRequest) throws JsonProcessingException {
         // Build Prompt
         String prompt = buildPrompt(emailRequest);
         // Prepare raw JSON Body
@@ -35,9 +39,33 @@ public class EmailGeneratorService {
                   }
                 """, prompt);
         // Send Request
-
+        String response = webClient.post()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/v1beta/models/gemini-2.5-flash:generateContent")
+                                .build())
+                .header("x-goog-api-key", apiKey)
+                .header("Content-Type","application/json" )
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
         // Extract Response
+        return extractResponseContent(response);
     }
+
+    private String extractResponseContent(String response) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(response);
+        return root.path("candidates")
+                .get(0)
+                .path("content")
+                .path("parts")
+                .get(0)
+                .path("text")
+                .asText();
+    }
+
+
 
     private String buildPrompt(EmailRequest emailRequest) {
         StringBuilder prompt = new StringBuilder();
